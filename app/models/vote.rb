ce1dfb3e95e58d6a3bd7aa9e5e6ac811ba3214de
Vinicius Zuchi <liquimaster@gmail.com>
2023-06-27 00:27:51 +0200

    fix rubocop and add some frozen_string_literal:true comments

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,9 +1,9 @@
+# frozen_string_literal: true
+
 class Vote < ApplicationRecord
   belongs_to :choice
   belongs_to :poll
   belongs_to :user
 
   validates :user_id, uniqueness: { scope: :poll }
-  
-  
 end
